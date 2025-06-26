199b631ab2df52b32bfc5f5b34404e7efba4bc94
sameena-rgb <saminnunf@gmail.com>
2025-06-26 10:57:49 +0530

    login module completed successfully

---
+++ addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+public static void main(String args[])
+{
+int n1=90;
+int n2=70;
+int add=n1+n2;
+system.out.print("Hello GitHub");
+system.out.print("Adiition of two numbers="+add);
+}
